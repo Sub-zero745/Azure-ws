22e88c8838695edec85c6e430fb0bebde6057e5b
Sub-zero745 <Subzero70000000@gmail.com>
2025-08-26 21:39:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = net.createServer(socket => {
   console.log('⚡️ Nueva conexión TCP entrante');
     
    const PORT = 22;
-   const IP = '127.0.0.1';
+   const IP = '5.34.178.42';
 
   // Conexión al servidor SSH
   const ssh = net.connect({ host: IP, port: PORT }, () => {
